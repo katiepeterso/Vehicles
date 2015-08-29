724e3f3e32007ec23bc13e05c9328984c67663c1
katiepeterso <katiepeterso@outlook.com>
2015-08-29 09:57:54 -0700

    setting up car subclass

---
+++ Vehicles/Car.h
@@ -0,0 +1,18 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Katherine Peterson on 2015-08-29.
+//  Copyright (c) 2015 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+@end
